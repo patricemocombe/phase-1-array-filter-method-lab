eef5b6bda7bce839660a0b99e22bfc599d7c08f3
patrice mocombe <patricemocombe@gmail.com>
2022-07-05 20:22:10 -0400

    commit tree d6a3b2a63a13e305f8b118274dd4e6dcbb43f2d3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (drivers, name) {
+    return drivers.filter(
+        (param) => param.toLowerCase() === name.toLowerCase()
+    );
+} 
+
+function fuzzyMatch(drivers, name2) {
+    return drivers.filter(
+        (param) =>
+        param.toLowerCase().indexOf(name2.toLowerCase()) === 0
+    );
+    }
+
+
+function matchName(drivers, name3) {
+    return drivers.filter((record) => record.name === name3);
+
+}
